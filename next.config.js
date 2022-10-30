9997de26ce6994f697844514d9938f40135971be
wutong21 <18072916289@163.com>
2022-10-30 12:18:24 +0800

    create blog

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  // swcMinify: true,
+  images: {
+    domains: ['media.graphassets.com'],
+    unoptimized: true,
+  },
 }
 
 module.exports = nextConfig
